eebfe3fff90779ae40bf74c3bf4a33739d2880e6
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-10-07 13:54:54 +0000

    Record a Signed state for Builds and propagate that to Updates
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

---
+++ alembic/versions/3cde3882442a_add_signed_column_to_builds.py
@@ -0,0 +1,24 @@
+"""Add a signed column to the Builds model.
+
+Revision ID: 3cde3882442a
+Revises: 4df1fcd59050
+Create Date: 2016-10-11 19:37:30.847965
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3cde3882442a'
+down_revision = '4df1fcd59050'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('builds', sa.Column('signed', sa.Boolean(), nullable=True, default=False))
+    op.execute('''UPDATE "builds" SET signed=0;''')
+    op.alter_column('builds', column_name='signed', nullable=False)
+
+
+def downgrade():
+    op.drop_column('builds', 'signed')
